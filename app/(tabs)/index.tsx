d322f1775126c7ad44417548d8fbc14091bc9eda
BrayanE02 <escb1122@gmail.com>
2024-12-10 01:11:06 -0700

    Log in set up but buttons dont work

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello, World!</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Social Media App</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Email" />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Password" />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Log In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.link}>Create New Account?</Text>
+        </TouchableOpacity>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -14,9 +30,39 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'black',
   },
   title: {
-    fontSize: 24,
+    fontSize: 32,
     fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#D3D3D3',
+    borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#A9A9A9',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  link: {
+    color: 'white',
+    marginTop: 20,
+    textDecorationLine: 'underline',
   },
 });
